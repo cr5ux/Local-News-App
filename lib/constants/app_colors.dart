2db345096220bc38d464a86040c15747b54315eb
Naol22 <Naolgirma21@gmail.com>
2025-05-31 16:57:49 +0300

    Default colors and category typo

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF41001F);
+  static const Color accent = Color(0xFFE91E63);
+  static const Color background = Colors.white;
+  static const Color text = Colors.black;
+  // Add more as needed
+}
