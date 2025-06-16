7d5ddb4ecfef8cb3abdb497b57a995ae14510ba5
cr5ux <armatemsamuel@gmail.com>
2025-06-16 21:09:20 +0300

    update

--- integration_test/app_test.dart
+++
@@ -1,9 +0,0 @@
-
-import 'package:localnewsapp/main.dart' as app;
-import 'package:integration_test/integration_test.dart';
-
-void main()
-{
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-  app.main();
-}
