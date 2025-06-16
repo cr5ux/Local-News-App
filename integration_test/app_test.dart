f743d352d378e29ebabc383a690ca310e6a6a766
cr5ux <armatemsamuel@gmail.com>
2025-06-16 20:32:29 +0300

    let us try something

---
+++ integration_test/app_test.dart
@@ -0,0 +1,9 @@
+
+import 'package:localnewsapp/main.dart' as app;
+import 'package:integration_test/integration_test.dart';
+
+void main()
+{
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  app.main();
+}
